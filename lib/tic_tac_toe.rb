cf78838bdff5545f9ffd98973fa0abc97fc5ad0a
Kelsey Fecho <kfecho30@gmail.com>
2018-03-04 22:20:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,8 +11,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player(board))
-  board[index] = current_player(board)
+def move(board, index, token)
+  board[index] = token
 end
 
 def position_taken?(board, location)
